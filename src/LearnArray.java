91fa84be8a37ceda03431b3f26bbb8e3fa093623
hwrdyen <hwrdpvt@gmail.com>
2024-09-23 11:34:16 +0800

    Initialized java-dsa

---
+++ src/LearnArray.java
@@ -0,0 +1,14 @@
+public class LearnArray {
+
+    public static void print() {
+        System.out.println("--Learn Array--");
+    }
+
+    public static void arrayExample() {
+        System.out.println("arrayExample 1:");
+        int[] intArray = new int[7]; //Array need to specify size (not dynamic)
+        intArray[0] = 20;
+        intArray[1] = 20;
+    }
+
+}
